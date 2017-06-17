88a2b87727d312f58fe64c1b339c8e3407398edb
Dogu <ddu@oriens.co>
2017-06-17 15:29:56 +0300

    Complete v1.0
    MVP, ready for deployment

--- app/src/main/java/kiddo/epsilon/custodet/MainActivity.java
+++ app/src/main/java/kiddo/epsilon/custodet/MainActivity.java
@@ -18,7 +18,6 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
@@ -60,8 +59,8 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     private ImageSurfaceView mImageSurfaceView;
     private Camera camera;
 
-    private FrameLayout cameraPreviewLayout;
-    private ImageView capturedImageHolder;
+    private FrameLayout cameraPreviewLayoutLeft;
+    private FrameLayout cameraPreviewLayoutRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,11 +104,15 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         //Set the image of image view to temp photo
         photoBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.fountain_pen); // Temporary bitmap
 
-        //Camera Preview
-        cameraPreviewLayout = (FrameLayout)findViewById(R.id.camera_preview);
-        capturedImageHolder = (ImageView)findViewById(R.id.captured_image);
+        //CAMERA VIEW (Not for blind)
+        //Initialize frame layout
+        cameraPreviewLayoutLeft = (FrameLayout)findViewById(R.id.camera_preview_left);
+        cameraPreviewLayoutRight = (FrameLayout) findViewById(R.id.camera_preview_right);
+        //Initialize surface view
         mImageSurfaceView = new ImageSurfaceView(MainActivity.this, camera);
-        cameraPreviewLayout.addView(mImageSurfaceView);
+        //Show surface view from frame layout
+        cameraPreviewLayoutLeft.addView(mImageSurfaceView);
+        cameraPreviewLayoutRight.addView(mImageSurfaceView);
 
     }
 
@@ -133,15 +136,16 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
             photoBitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-            if(photoBitmap==null){
-                Toast.makeText(MainActivity.this, "Captured image is empty", Toast.LENGTH_LONG).show();
+            if(photoBitmap==null){ // If a problem occurs with taking photo
+                Speak("Fotoğraf çekilemedi...");
+                state=0;
                 return;
             }
-            capturedImageHolder.setImageBitmap(scaleDownBitmapImage(photoBitmap, 300, 200 ));
             ProcessImage();
         }
     };
 
+    // (For displaying purposes) Bitmap rescale bitmap method
     private Bitmap scaleDownBitmapImage(Bitmap bitmap, int newWidth, int newHeight){
         Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
         return resizedBitmap;
@@ -158,6 +162,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
             new DoRequest().execute();
         } catch (Exception e) {
             Speak("Hata var.");
+            state=0;
         }
     }
 
@@ -190,6 +195,8 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                 return Process();
             } catch (Exception e) {
                 this.e = e;    // Store error
+                Speak("Hata var.");
+                state=0;
             }
 
             return null;
@@ -207,6 +214,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
             if (e != null) {
                 Speak("Hata var.");
                 this.e = null;
+                state=0;
             } else {
                 Gson gson = new Gson();
                 AnalysisResult result = gson.fromJson(data, AnalysisResult.class);
@@ -310,222 +318,3 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         super.onPause();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
-    //After the photo is taken
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-        imageTakenPhoto.setImageBitmap(bitmap);
-    }
-
-    **/
-/**
- * INTENT METHOD FOR TAKING PHOTO (OBSLETE)
- * //Copied mostly from https://developer.android.com/training/camera/photobasics.html
- * //Intent for taking photo
- * private void dispatchTakePictureIntent() {
- * Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
- * //Ensure that there is a camera activity to handle the intent
- * if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
- * <p>
- * // Create the File where the photo should go
- * File photoFile = null;
- * try {
- * photoFile = createImageFile();
- * } catch (IOException ex) {
- * // Error occurred while creating the File
- * }
- * <p>
- * // Continue only if the File was successfully created
- * if (photoFile != null) {
- * Uri photoURI = FileProvider.getUriForFile(this,
- * "kiddo.epsilon.custodet.fileprovider",
- * photoFile);
- * takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
- * startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
- * }
- * <p>
- * }
- * }
- * <p>
- * //Creating UNIQUE file name of image
- * private File createImageFile() throws IOException {
- * // Create an image file name
- * String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
- * String imageFileName = "JPEG_" + timeStamp + "_";
- * File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
- * File image = File.createTempFile(
- * imageFileName,  // prefix
- * ".jpg",         // suffix
- * storageDir      // directory
- * );
- * <p>
- * // Save a file: path for use with ACTION_VIEW intents
- * currentPhotoPath = image.getAbsolutePath();
- * return image;
- * }
- * <p>
- * //Alternative method
- * void TakePicture() {
- * Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // Create intent for capturing image
- * startActivityForResult(intent, 0);
- * }
- */
-
-// Copied from https://www.youtube.com/watch?v=RKyl2Ko0Ag0&t=182s
-    /*void ProcessImage() {
-        //Image as a bitmap
-        //photoBitmap = BitmapFactory.decodeResource(getResources(),R.drawable.fountain_pen);
-
-        //Convert image to stream
-        ByteArrayOutputStream imageOutputStream = new ByteArrayOutputStream();
-        photoBitmap.compress(Bitmap.CompressFormat.JPEG, 100, imageOutputStream);
-        final ByteArrayInputStream imageInputStream = new ByteArrayInputStream(imageOutputStream.toByteArray());
-
-        //Main task to analyze image (?)AsyncTask)
-        final AsyncTask<InputStream, String, String> visionTask = new AsyncTask<InputStream, String, String>() {
-            ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
-
-            @Override
-            protected String doInBackground(InputStream... params) {
-                try {
-                    publishProgress("Algılanıyor");
-                    String[] features = {"Açıklama"};
-                    String[] details = {};
-
-                    AnalysisResult resultOfAnalysis = visionServiceClient.analyzeImage(params[0], features, details);
-                    String stringOfAnalysis = new Gson().toJson(resultOfAnalysis);
-                    return stringOfAnalysis;
-
-                } catch (Exception e) { // For every exception
-                    return null;
-                }
-            }
-
-            //Before executing AsyncTask
-            @Override
-            protected void onPreExecute() {
-                progressDialog.show();
-            }
-
-            //After executing AsyncTask
-            @Override
-            protected void onPostExecute(String s) {
-                progressDialog.dismiss();
-                AnalysisResult resultOfAnalysis = new Gson().fromJson(s, AnalysisResult.class);
-
-                //String builder
-                StringBuilder resultStringBuilder = new StringBuilder();
-                for (Caption caption : resultOfAnalysis.description.captions) {
-                    resultStringBuilder.append(caption.text);
-                }
-
-                //Pass the builded string to the string which will be read from Text-to-Speech
-                textToSpeechInputText = resultStringBuilder.toString();
-            }
-
-            //When the progress of AsyncTask is updated
-            @Override
-            protected void onProgressUpdate(String... values) {
-                progressDialog.setMessage(values[0]);
-                //Text-to-speech progress
-                Speak(values[0].toString()); // (?) is toString necessary
-            }
-
-        };
-
-        //Calling AsyncTask
-        visionTask.execute(imageInputStream);
-    }*/
-
-/*private CameraDevice cameraDevice;
-    private CameraDevice.StateCallback cameraDeviceStateCallBack
-            = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(@NonNull CameraDevice camera) {
-
-        }
-
-        @Override
-        public void onDisconnected(@NonNull CameraDevice camera) {
-
-        }
-
-        @Override
-        public void onError(@NonNull CameraDevice camera, int error) {
-
-        }
-    };*/
-//TAKING PHOTO
-//Copied from https://developer.android.com/guide/topics/media/camera.html#custom-camera
-    /*
-    //Accessing camera
-    public static Camera getCameraInstance() {
-        Camera c = null;
-        try {
-
-            c = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK); // attempt to get a Camera instance
-            Log.e("Camera: ","Camera opened succesfully");
-        } catch (Exception e) {
-            Log.e("Camera Error:","Can't open camera");
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
-    }
-
-    //Retrieving image from camera
-    private PictureCallback pictureCallback = new PictureCallback() {
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-            Log.e("Camera: ", "inside pictureCallBack onPictureTaken");
-            try {
-                // Replace bitmap variable with the new taken photo (transfer byte array to bitmap)
-                photoBitmap = BitmapFactory.decodeByteArray(data,0,data.length);
-                //Blinds can't see but for debug purposes replace image views with new photo
-                imageTakenPhoto.setImageBitmap(photoBitmap);
-                imageTakenPhoto2.setImageBitmap(photoBitmap);
-
-                //Continue with Image Processing
-                ProcessImage();
-                Speak("Algılanıyor...");
-            } catch (Exception e){
-                Speak("Hata var.");
-            }
-        }
-    };
-
-    //Take picture method
-    void CaptureImage() {
-        //Re-accsess camera if it has been released somehow
-        if (camera == null) {
-            camera = getCameraInstance();
-            Log.e("Camera: ","Camera initialized in Capture Image method");
-        }
-
-        camera.unlock();
-        camera.enableShutterSound(true); // SOund for shutter
-        camera.takePicture(null, null, pictureCallback); // Capture image from camera
-        Log.e("Camera:", "Take picture method executed");
-    }
-    */
